02ed4a0f7d2e9823f0306ac4988f8467aa0142bf
Yuti Jirawattanapalin <66056066@kmitl.ac.th>
2023-10-11 18:45:47 +0700

    Assignment

---
+++ src/ex07.py
@@ -0,0 +1,21 @@
+"""
+Execise 7
+"""
+
+
+def print_ASCII_table(start_char,end_char):
+    """
+    Generate and print the ASCII table for a range of characters.
+
+    Args:
+        start_char (int): The ASCII value of the starting character.
+        end_char (int): The ASCII value of the ending character.
+
+    Returns:
+        str: Returns nothing. The function simply prints the ASCII characters.
+
+    """
+    # TODO : complete this
+    for i in range(start_char,end_char+1):
+        print(chr(i))
+
