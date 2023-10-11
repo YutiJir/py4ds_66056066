3d65399ae9b3419b25bb8bf6503f206ea4c81a10
Yuti Jirawattanapalin <66056066@kmitl.ac.th>
2023-10-11 19:59:31 +0700

    Assignment

---
+++ src/ex13.py
@@ -0,0 +1,32 @@
+"""
+Exercise 13
+"""
+
+
+def calc_sum(num_list):
+    """
+    Calculate the sum of a list of numbers.
+
+    Parameters:
+    - num_list (list): A list of numbers.
+
+    Returns:
+    - int: The sum of all the numbers in the list.
+    """
+    return sum(num_list)
+
+
+def calc_prod(num_list):
+    """
+    Calculates the product of all the numbers in the given list.
+
+    Parameters:
+        num_list (list): A list of numbers.
+
+    Returns:
+        int: The product of all the numbers in the list.
+    """
+    result = 1
+    for i in num_list :
+        result = result * i
+    return result
