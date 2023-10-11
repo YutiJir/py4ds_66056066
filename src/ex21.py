b48a7cefec5312d47a3608c15bc64f592b951355
Yuti Jirawattanapalin <66056066@kmitl.ac.th>
2023-10-11 22:46:04 +0700

    Assignment

---
+++ src/ex21.py
@@ -0,0 +1,35 @@
+"""
+Exercise 21 : Validate Date
+"""
+
+
+def is_valid_date(year, month, day):
+    """
+    Check if a given date is valid.
+
+    Parameters:
+        year (int): The year of the date.
+        month (int): The month of the date.
+        day (int): The day of the date.
+
+    Returns:
+        bool: True if the date is valid, False otherwise.
+    """
+    # important to import here
+    from src.ex20 import is_leap_year
+
+    # TODO : complete this
+    if not (1 <= month <= 12):
+        return False
+
+    if is_leap_year(year) and month == 2 and day == 29:
+        return True
+
+    if month in (1,3,5,7,8,10,12) and (1 <= day <= 31):
+        return True
+    elif month in (4,6,9,11) and (1 <= day <= 30):
+        return True
+    elif month == 2 and (1 <= day <= 28):
+        return True
+
+    return False
