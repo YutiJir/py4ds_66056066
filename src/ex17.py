c4b6641441cfee6af169dd56ebcf589a7f00ca03
Yuti Jirawattanapalin <66056066@kmitl.ac.th>
2023-10-11 20:45:25 +0700

    Assignment

---
+++ src/ex17.py
@@ -0,0 +1,20 @@
+"""
+Exercise 17
+"""
+import random
+
+
+def roll_dice(num_of_dice):
+    """
+    Calculate the total sum of rolling a certain number of dice.
+
+    Parameters:
+        num_of_dice (int): The number of dice to roll.
+
+    Returns:
+        int: The total sum of the dice rolls.
+    """
+    total = 0
+    for i in range(num_of_dice):
+        total += random.randint(1,6)
+    return total
