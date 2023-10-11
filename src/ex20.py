1aa77055c9b0fe5f216c3746edff25e13b818b21
Yuti Jirawattanapalin <66056066@kmitl.ac.th>
2023-10-11 22:24:53 +0700

    Assignment

---
+++ src/ex20.py
@@ -0,0 +1,24 @@
+"""
+Exercise 20 : Leap Year
+"""
+
+
+def is_leap_year(year):
+    """
+    Check if the given year is a leap year.
+
+    Parameters:
+        year (int): The year to check.
+
+    Returns:
+        bool: True if the year is a leap year, False otherwise.
+    """
+    # TODO : complete this
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
