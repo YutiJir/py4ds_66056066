29ac7e26fe20124a2a8a5b1a1bc2b7f69d1ad8ce
Yuti Jirawattanapalin <66056066@kmitl.ac.th>
2023-10-11 18:52:38 +0700

    Assignment

---
+++ src/ex08.py
@@ -0,0 +1,50 @@
+"""
+Execise 8
+"""
+
+
+def write_to_file(filename, message):
+    """
+    Write the given message to a file with the provided filename.
+
+    Args:
+        filename (str): The name of the file to write to.
+        message (str): The message to write to the file.
+
+    Returns:
+        None
+    """
+    # TODO : complete this
+    with open(filename ,'w') as file:
+        file.write(message)
+
+
+def read_from_file(filename):
+    """
+    Read the contents of a file.
+
+    Args:
+        filename (str): The name of the file to read.
+
+    Returns:
+        str: The contents of the file.
+    """
+    # TODO : complete this
+    with open(filename) as file:
+        return file.read()
+
+
+def append_to_file(filename, message):
+    """
+    Append the given message to the end of the specified file.
+
+    Args:
+        filename (str): The name of the file to append the message to.
+        message (str): The message to append to the file.
+
+    Returns:
+        None: This function does not return anything.
+    """
+    # TODO : complete this
+    with open(filename,'a') as file:
+        file.write(message)
