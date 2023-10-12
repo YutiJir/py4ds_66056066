c2c2de0fed862bbe48a07339de73809e97807cc0
Yuti Jirawattanapalin <66056066@kmitl.ac.th>
2023-10-12 19:34:21 +0700

    Assignment

---
+++ src/ex24.py
@@ -0,0 +1,23 @@
+"""
+Exercise 24 : Every 15 minutes, ante meridiem (am) and post meridiem (pm)
+ante : before
+post : after
+meridiem : midday
+"""
+
+
+def get_time_every_15_min():
+    """
+    Generate a time string every 15 minutes.
+
+    This function iterates through the meridiems, hours, and minutes to generate a time string
+    for every 15 minutes. It prints the time string in the format "hour:minute meridiem".
+
+    Returns:
+        str: The generated time string.
+    """
+    # Fix : complete this
+    for meridiem in ['am', 'pm']:
+        for hour in ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
+            for minutes in ['00','15', '30', '45']:
+                print(hour + ':' + minutes + ' ' + meridiem)
